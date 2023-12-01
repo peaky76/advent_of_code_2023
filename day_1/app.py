465627c328a0dc9ea37fbfcd064f9d6d22b9ed5a
peaky76 <robertjamespeacock@gmail.com>
2023-12-01 15:57:51 +0000

    day 1 part 1 complete

--- day_1/app.py
+++ day_1/app.py
@@ -1,6 +1,14 @@
 import sys
 sys.path.append('..')
 
-from advent_of_code_2023 import read_input
+from advent_of_code_2023 import read_input  # noqa: E402
 
-print(read_input("./day_1/puzzle_input"))
+def get_calibration_value(line):
+    digits = [int(digit) for digit in line if digit.isdigit()]
+    first = digits[0]
+    last = digits[-1]
+    return int(str(f"{first}{last}"))
+
+# PART ONE
+# print(sum(get_calibration_value(line) for line in read_input("./day_1/example_input_1")))
+print(sum(get_calibration_value(line) for line in read_input("./day_1/puzzle_input")))
