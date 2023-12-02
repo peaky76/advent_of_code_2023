0c78467802e84a8fa4858a185703f775db5fb0bb
peaky76 <robertjamespeacock@gmail.com>
2023-12-02 08:58:29 +0000

    ruff formatting

--- advent_of_code_2023/day_2/app.py
+++ advent_of_code_2023/day_2/app.py
@@ -9,7 +9,11 @@ from advent_of_code_2023 import read_input  # noqa: E402
 
 def parse_grab(grab):
     def find(phrase):
-        return int(match.groups(0)[0]) if (match := re.search(f"(\\d+) {phrase}", grab)) else 0
+        return (
+            int(match.groups(0)[0])
+            if (match := re.search(f"(\\d+) {phrase}", grab))
+            else 0
+        )
 
     return (find("red"), find("green"), find("blue"))
 
@@ -19,26 +23,37 @@ def parse_input(line):
     grabs = line.split(":")[1].split(";")
     return {"id": the_id, "rgb": [parse_grab(grab) for grab in grabs]}
 
+
 def min_possible_balls(grabs):
     reds = [grab[0] for grab in grabs]
     greens = [grab[1] for grab in grabs]
     blues = [grab[2] for grab in grabs]
     return (max(reds), max(greens), max(blues))
 
+
 def is_grab_too_much(grab_rgb, limit_rgb):
     return any(grab > limit for grab, limit in zip(grab_rgb, limit_rgb))
+
 
 def is_game_possible(grabs, limit_rgb):
     return all(not is_grab_too_much(grab_rgb, limit_rgb) for grab_rgb in grabs)
 
+
 def sum_of_possible_games(lines, limit_rgb=(12, 13, 14)):
-    return sum(game["id"] for game in get_games(lines) if is_game_possible(game["rgb"], limit_rgb))
+    return sum(
+        game["id"]
+        for game in get_games(lines)
+        if is_game_possible(game["rgb"], limit_rgb)
+    )
+
 
 def power(grab_rgb):
     return math.prod(grab_rgb)
 
+
 def get_games(input):
     return [parse_input(line) for line in input]
+
 
 # PART ONE
 example_answer = sum_of_possible_games(read_input("./day_2/example_input"))
@@ -47,7 +62,13 @@ puzzle_answer = sum_of_possible_games(read_input("./day_2/puzzle_input"))
 print(f"Puzzle answer: {puzzle_answer}")
 
 # # PART TWO
-example_answer = sum(power(min_possible_balls(game["rgb"])) for game in get_games(read_input("./day_2/example_input")))
+example_answer = sum(
+    power(min_possible_balls(game["rgb"]))
+    for game in get_games(read_input("./day_2/example_input"))
+)
 print(f"Example answer: {example_answer}")
-puzzle_answer = sum(power(min_possible_balls(game["rgb"])) for game in get_games(read_input("./day_2/puzzle_input")))
+puzzle_answer = sum(
+    power(min_possible_balls(game["rgb"]))
+    for game in get_games(read_input("./day_2/puzzle_input"))
+)
 print(f"Puzzle answer: {puzzle_answer}")
