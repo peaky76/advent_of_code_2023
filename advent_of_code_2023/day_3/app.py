4f4b2125bc94e44db6456c7d885363cc387fb95e
peaky76 <robertjamespeacock@gmail.com>
2023-12-03 12:43:32 +0000

    day 3 is_adjacengt and has_adjacent_indicator fns

--- advent_of_code_2023/day_3/app.py
+++ advent_of_code_2023/day_3/app.py
@@ -31,6 +31,18 @@ def get_part_numbers(lines):
 def get_part_indicators(lines):
     return [(i, j) for i, line in enumerate(lines) for j, char in enumerate(line) if not char.isdigit() and not char == '.']
 
+def has_adjacent_indicator(part_loc_and_len, part_indicators):
+    x, y, length = part_loc_and_len
+    coords = [(x, i) for i in range(y, y + length)]
+    if any(is_adjacent(part_bit, part_indicator) for part_bit in coords for part_indicator in part_indicators):        
+        return True
+    return False
+
+def is_adjacent(coord_1, coord_2):
+    x_1, y_1 = coord_1
+    x_2, y_2 = coord_2
+    return abs(x_1 - x_2) <= 1 and abs(y_1 - y_2) <= 1
+
 # PART ONE
 # example_answer = the_function(read_input("./day_3/example_input"))
 # print(f"Example answer: {example_answer}")
--- advent_of_code_2023/day_3/test_app.py
+++ advent_of_code_2023/day_3/test_app.py
@@ -1,4 +1,5 @@
-from .app import get_part_numbers, get_part_indicators
+import pytest
+from .app import get_part_numbers, get_part_indicators, has_adjacent_indicator, is_adjacent
 
 def test_get_part_numbers():
     lines = ["123...",".45.67","..890."]
@@ -16,3 +17,32 @@ def test_get_part_indicators():
     expected = get_part_indicators(lines)
     actual = [(0, 4), (2, 2)]
     assert expected == actual
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ([(3, 3), (3, 4)], True),
+        ([(3, 3), (2, 3)], True), 
+        ([(3, 3), (3, 2)], True),
+        ([(3, 3), (4, 3)], True),
+        ([(3, 3), (4, 4)], True),
+        ([(3, 3), (3, 5)], False),
+        ([(3, 3), (5, 3)], False),
+    ],
+)
+def test_is_adjacent(test_input, expected):
+    test_coord_1, test_coord_2 = test_input
+    assert is_adjacent(test_coord_1, test_coord_2) == expected
+    
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ([(0, 0, 3), [(1, 3), (2, 6), (3,3), (4, 5)]], True),
+        ([(0, 5, 3), [(1, 3), (2, 6), (3,3), (4, 5)]], False),
+        ([(2, 2, 2), [(1, 3), (2, 6), (3,3), (4, 5)]], True),
+        ([(2, 6, 3), [(1, 3), (2, 6), (3,3), (4, 5)]], True),
+    ],
+)
+def test_has_adjacent_indicator(test_input, expected):
+    part_loc_and_len, part_indicators = test_input
+    assert has_adjacent_indicator(part_loc_and_len, part_indicators) == expected
