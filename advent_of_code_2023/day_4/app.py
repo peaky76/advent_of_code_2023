2eeee36fc6fc3b7ef2416d3c0042c4adcde16722
peaky76 <robertjamespeacock@gmail.com>
2023-12-04 19:27:15 +0000

    day 4 part 1 complete

--- advent_of_code_2023/day_4/app.py
+++ advent_of_code_2023/day_4/app.py
@@ -4,7 +4,7 @@ import sys
 
 sys.path.append("..")
 
-# from advent_of_code_2023 import read_input  # noqa: E402
+from advent_of_code_2023 import read_input  # noqa: E402
 
 def parse_line(line):
     return {
@@ -17,11 +17,16 @@ def calculate_points(scratchcard):
     wins = sum(num in scratchcard["winning_nums"] for num in scratchcard["selected_nums"])
     return 1 * (2 ** (wins  - 1)) if wins else 0
 
+def assess_scratchcards(file):
+    lines = read_input(file)
+    scratchcards = [parse_line(line) for line in lines]
+    return sum(calculate_points(scratchcard) for scratchcard in scratchcards)
+
 # PART ONE
-# example_answer = the_function("./day_4/example_input")
-# print(f"Example answer: {example_answer}")
-# puzzle_answer = the_function("./day_4/puzzle_input")
-# print(f"Puzzle answer: {puzzle_answer}")
+example_answer = assess_scratchcards("./day_4/example_input")
+print(f"Example answer: {example_answer}")
+puzzle_answer = assess_scratchcards("./day_4/puzzle_input")
+print(f"Puzzle answer: {puzzle_answer}")
 
 # PART TWO
 # example_answer = the_function("./day_4/example_input")
