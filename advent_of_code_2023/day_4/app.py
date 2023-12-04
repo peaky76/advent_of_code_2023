771c2ca9d4e55d2b63741beb49bbc485c4bfb841
peaky76 <robertjamespeacock@gmail.com>
2023-12-04 19:55:14 +0000

    day 4 complete

--- advent_of_code_2023/day_4/app.py
+++ advent_of_code_2023/day_4/app.py
@@ -1,5 +1,6 @@
 # Day 4: Scratchcards
 
+from collections import defaultdict
 import sys
 
 sys.path.append("..")
@@ -8,19 +9,36 @@ from advent_of_code_2023 import read_input  # noqa: E402
 
 def parse_line(line):
     return {
-        "card": int(line.split(":")[0].split("Card ")[1]),
+        "card_id": int(line.split(":")[0].split("Card ")[1]),
         "winning_nums": [int(num) for num in line.split(":")[1].split("|")[0].split()],
         "selected_nums": [int(num) for num in line.split(":")[1].split("|")[1].split()],
     }    
 
 def calculate_points(scratchcard):
-    wins = sum(num in scratchcard["winning_nums"] for num in scratchcard["selected_nums"])
-    return 1 * (2 ** (wins  - 1)) if wins else 0
+    return 1 * (2 ** (wins  - 1)) if (wins := calculate_wins(scratchcard)) else 0
+
+def calculate_wins(scratchcard):
+    return sum(num in scratchcard["winning_nums"] for num in scratchcard["selected_nums"])
 
 def assess_scratchcards(file):
+    return sum(val for key, val in get_scratchcard_scores(file).items())
+
+def get_scratchcard_scores(file, *, use_points = True):
     lines = read_input(file)
     scratchcards = [parse_line(line) for line in lines]
-    return sum(calculate_points(scratchcard) for scratchcard in scratchcards)
+    totalling_function = calculate_points if use_points else calculate_wins
+    return {scratchcard['card_id']: totalling_function(scratchcard) for scratchcard in scratchcards }
+
+def count_copies(file):
+    copy_count = defaultdict(int)
+    scratchcard_scores = get_scratchcard_scores(file, use_points = False)
+    for key, val in scratchcard_scores.items():
+        copy_count[key] += 1
+        for i in range(1, val + 1):
+            print(key, i)
+            copy_count[key + i] += copy_count[key]
+    return sum(copy_count.values())
+
 
 # PART ONE
 example_answer = assess_scratchcards("./day_4/example_input")
@@ -29,7 +47,7 @@ puzzle_answer = assess_scratchcards("./day_4/puzzle_input")
 print(f"Puzzle answer: {puzzle_answer}")
 
 # PART TWO
-# example_answer = the_function("./day_4/example_input")
-# print(f"Example answer: {example_answer}")
-# puzzle_answer = the_function("./day_4/puzzle_input")
-# print(f"Puzzle answer: {puzzle_answer}")
+example_answer = count_copies("./day_4/example_input")
+print(f"Example answer: {example_answer}")
+puzzle_answer = count_copies("./day_4/puzzle_input")
+print(f"Puzzle answer: {puzzle_answer}")
--- advent_of_code_2023/day_4/test_app.py
+++ advent_of_code_2023/day_4/test_app.py
@@ -5,11 +5,11 @@ from .app import calculate_points, parse_line
     "test_input,expected",
     [
         ("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", {
-            "card": 1, 
+            "card_id": 1, 
             "winning_nums": [41, 48, 83, 86, 17], 
             "selected_nums": [83, 86, 6, 31, 17, 9, 48, 53]}),
         ("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", {
-            "card": 6, 
+            "card_id": 6, 
             "winning_nums": [31, 18, 13, 56, 72],
             "selected_nums": [74, 77, 10, 23, 35, 67, 36, 11]}),
     ],
@@ -22,11 +22,11 @@ def test_parse_line(test_input, expected):
     "test_input,expected",
     [
         ({
-            "card": 1, 
+            "card_id": 1, 
             "winning_nums": [41, 48, 83, 86, 17], 
             "selected_nums": [83, 86, 6, 31, 17, 9, 48, 53]}, 8),
         ({
-            "card": 6, 
+            "card_id": 6, 
             "winning_nums": [31, 18, 13, 56, 72],
             "selected_nums": [74, 77, 10, 23, 35, 67, 36, 11]}, 0),
     ],
