0c506fa5c7a897ca9b5ebe97b9985fe952874fe4
peaky76 <robertjamespeacock@gmail.com>
2023-12-04 19:17:23 +0000

    day 4 parse line

--- advent_of_code_2023/day_4/app.py
+++ advent_of_code_2023/day_4/app.py
@@ -4,20 +4,23 @@ import sys
 
 sys.path.append("..")
 
-from advent_of_code_2023 import read_input  # noqa: E402
+# from advent_of_code_2023 import read_input  # noqa: E402
 
-def the_function(file):
-    lines = read_input(file)
-    
+def parse_line(line):
+    return {
+        "card": int(line.split(":")[0].split("Card ")[1]),
+        "winning_nums": [int(num) for num in line.split(":")[1].split("|")[0].split()],
+        "selected_nums": [int(num) for num in line.split(":")[1].split("|")[1].split()],
+    }    
 
 # PART ONE
-example_answer = the_function("./day_4/example_input")
-print(f"Example answer: {example_answer}")
-puzzle_answer = the_function("./day_4/puzzle_input")
-print(f"Puzzle answer: {puzzle_answer}")
+# example_answer = the_function("./day_4/example_input")
+# print(f"Example answer: {example_answer}")
+# puzzle_answer = the_function("./day_4/puzzle_input")
+# print(f"Puzzle answer: {puzzle_answer}")
 
 # PART TWO
-example_answer = the_function("./day_4/example_input")
-print(f"Example answer: {example_answer}")
-puzzle_answer = the_function("./day_4/puzzle_input")
-print(f"Puzzle answer: {puzzle_answer}")
+# example_answer = the_function("./day_4/example_input")
+# print(f"Example answer: {example_answer}")
+# puzzle_answer = the_function("./day_4/puzzle_input")
+# print(f"Puzzle answer: {puzzle_answer}")
--- advent_of_code_2023/day_4/test_app.py
+++ advent_of_code_2023/day_4/test_app.py
@@ -1,14 +1,20 @@
 import pytest
-from .app import the_function
+from .app import parse_line
 
 @pytest.mark.parametrize(
     "test_input,expected",
     [
-        (None, None),
+        ("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", {
+            "card": 1, 
+            "winning_nums": [41, 48, 83, 86, 17], 
+            "selected_nums": [83, 86, 6, 31, 17, 9, 48, 53]}),
+        ("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", {
+            "card": 6, 
+            "winning_nums": [31, 18, 13, 56, 72],
+            "selected_nums": [74, 77, 10, 23, 35, 67, 36, 11]}),
     ],
 )
-def test_the_function(test_input, expected):
-    actual = the_function(test_input)
-    expected = None
+def test_parse_line(test_input, expected):
+    actual = parse_line(test_input)
     assert actual == expected
     
