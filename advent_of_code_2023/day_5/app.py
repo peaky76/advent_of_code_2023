aa8d549f906e05b0b992873c8149b1c0314b84c0
peaky76 <robertjamespeacock@gmail.com>
2023-12-05 20:15:57 +0000

    day 5 parse line

--- advent_of_code_2023/day_5/app.py
+++ advent_of_code_2023/day_5/app.py
@@ -4,20 +4,25 @@ import sys
 
 sys.path.append("..")
 
-from advent_of_code_2023 import read_input  # noqa: E402
+# from advent_of_code_2023 import read_input  # noqa: E402
 
-def the_function(file):
-    lines = read_input(file)
+
+def parse_line(line):
+    destination_start, source_start, range_length = (int(x) for x in line.split(" "))
+    return (source_start, source_start + range_length - 1, destination_start - source_start)
+
+# def the_function(file):
+#     lines = read_input(file)
     
 
 # PART ONE
-example_answer = the_function("./day_4/example_input")
-print(f"Example answer: {example_answer}")
-puzzle_answer = the_function("./day_4/puzzle_input")
-print(f"Puzzle answer: {puzzle_answer}")
+# example_answer = the_function("./day_4/example_input")
+# print(f"Example answer: {example_answer}")
+# puzzle_answer = the_function("./day_4/puzzle_input")
+# print(f"Puzzle answer: {puzzle_answer}")
 
 # PART TWO
-example_answer = the_function("./day_4/example_input")
-print(f"Example answer: {example_answer}")
-puzzle_answer = the_function("./day_4/puzzle_input")
-print(f"Puzzle answer: {puzzle_answer}")
+# example_answer = the_function("./day_4/example_input")
+# print(f"Example answer: {example_answer}")
+# puzzle_answer = the_function("./day_4/puzzle_input")
+# print(f"Puzzle answer: {puzzle_answer}")
--- advent_of_code_2023/day_5/test_app.py
+++ advent_of_code_2023/day_5/test_app.py
@@ -1,14 +1,15 @@
-# import pytest
-# from .app import the_function
+import pytest
+from .app import parse_line
 
-# @pytest.mark.parametrize(
-#     "test_input,expected",
-#     [
-#         (None, None),
-#     ],
-# )
-# def test_the_function(test_input, expected):
-#     actual = the_function(test_input)
-#     expected = None
-#     assert actual == expected
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("50 98 2", (98, 99, -48)),
+        ("42 0 7", (0, 6, 42)),
+        ("18 25 70", (25, 94, -7)),
+    ],
+)
+def test_parse_line(test_input, expected):
+    actual = parse_line(test_input)
+    assert actual == expected
     
