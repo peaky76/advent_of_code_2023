ac5526cfaaa828a0615478b80f777e221748272f
peaky76 <robertjamespeacock@gmail.com>
2023-12-06 22:17:33 +0000

    day 6 setup

---
+++ advent_of_code_2023/day_6/test_app.py
@@ -0,0 +1,14 @@
+# import pytest
+# from .app import the_function
+
+# @pytest.mark.parametrize(
+#     "test_input,expected",
+#     [
+#         (None, None),
+#     ],
+# )
+# def test_the_function(test_input, expected):
+#     actual = the_function(test_input)
+#     expected = None
+#     assert actual == expected
+    
