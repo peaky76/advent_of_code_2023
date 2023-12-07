97782fc53f2a30b693dbad85af37c4ffa181e882
peaky76 <robertjamespeacock@gmail.com>
2023-12-07 22:31:31 +0000

    day 7 hand grouping

--- advent_of_code_2023/day_7/app.py
+++ advent_of_code_2023/day_7/app.py
@@ -1,23 +1,34 @@
 # Day 7: Camel Cards
 
+from collections import Counter
+
+# Ranking
+# Five of a kind, where all five cards have the same label: AAAAA
+# Four of a kind, where four cards have the same label and one card has a different label: AA8AA
+# Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
+# Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+# Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+# One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+# High card, where all cards' labels are distinct: 23456
+
 import sys
 
 sys.path.append("..")
 
-from advent_of_code_2023 import read_input  # noqa: E402
+# from advent_of_code_2023 import read_input  # noqa: E402
 
-def the_function(file):
-    lines = read_input(file)
+def hand_grouping(hand):
+    return tuple(sorted(Counter(list(hand)).values(), reverse=True))
     
 
 # PART ONE
-example_answer = the_function("./day_7/example_input")
-print(f"Example answer: {example_answer}")
-puzzle_answer = the_function("./day_7/puzzle_input")
-print(f"Puzzle answer: {puzzle_answer}")
+# example_answer = the_function("./day_7/example_input")
+# print(f"Example answer: {example_answer}")
+# puzzle_answer = the_function("./day_7/puzzle_input")
+# print(f"Puzzle answer: {puzzle_answer}")
 
-# PART TWO
-example_answer = the_function("./day_7/example_input")
-print(f"Example answer: {example_answer}")
-puzzle_answer = the_function("./day_7/puzzle_input")
-print(f"Puzzle answer: {puzzle_answer}")
+# # PART TWO
+# example_answer = the_function("./day_7/example_input")
+# print(f"Example answer: {example_answer}")
+# puzzle_answer = the_function("./day_7/puzzle_input")
+# print(f"Puzzle answer: {puzzle_answer}")
--- advent_of_code_2023/day_7/test_app.py
+++ advent_of_code_2023/day_7/test_app.py
@@ -1,14 +1,19 @@
-# import pytest
-# from .app import the_function
+import pytest
+from .app import hand_grouping
 
-# @pytest.mark.parametrize(
-#     "test_input,expected",
-#     [
-#         (None, None),
-#     ],
-# )
-# def test_the_function(test_input, expected):
-#     actual = the_function(test_input)
-#     expected = None
-#     assert actual == expected
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+       ('AAAAA', (5,)),
+       ('AA8AA', (4, 1)),
+       ('23332', (3, 2)),
+       ('TTT98', (3, 1, 1)),
+       ('23432', (2, 2, 1)),
+       ('A23A4', (2, 1, 1, 1)),
+       ('23456', (1, 1, 1, 1, 1))
+    ],
+)
+def test_hand_grouping(test_input, expected):
+    actual = hand_grouping(test_input)
+    assert actual == expected
     
